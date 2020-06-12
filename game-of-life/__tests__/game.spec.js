f9ddcffa5eede89ba4c7e1dfe1ab7ab5fc3370bd
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-06-11 22:46:02 -0400

    finished isAlive algorithm

--- game-of-life/__tests__/game.spec.js
+++ game-of-life/__tests__/game.spec.js
@@ -1,2 +1,19 @@
 require("../game");
 const { isAlive } = window.game;
+
+describe('game of life', ()=>{
+    describe('isAlive algorithm', ()=> {
+        test('dead cell with no neighbors', ()=> {
+            expect(isAlive(0,0)).toEqual(0)
+        })
+        test('dead cell with 3 neighbors should return 1', ()=>{
+            expect(isAlive(0,3)).toEqual(1)
+        })
+        test('live cell with 0 neighbors should return 0', ()=>{
+            expect(isAlive(1,0)).toEqual(0)
+        })
+        test('live cell with 2 neighbrs should return 1', ()=>{
+            expect(isAlive(1,2)).toEqual(1)
+        })
+    })
+})
--- game-of-life/game.js
+++ game-of-life/game.js
@@ -1,3 +1,8 @@
+
+const isAlive = (cell, neighbors) => (
+  neighbors === 3 || cell === 1 && neighbors ===2 ? 1 : 0
+)
+
 window.game = {
   isAlive
 };
